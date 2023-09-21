34cdc3e7822c9f03075771858b8eb4bc0d2db3e3
unknown <koos@cesbit.com>
2023-09-21 16:44:37 +0200

    enums

--- lib/check/idrac.py
+++ lib/check/idrac.py
@@ -7,6 +7,42 @@ QUERIES = (
     MIB_INDEX['IDRAC-MIB-SMIv2']['eventLogTableEntry'],
     MIB_INDEX['IDRAC-MIB-SMIv2']['firmwareTableEntry'],
 )
+ObjectStatusEnum = MIB_INDEX['IDRAC-MIB-SMIv2'][None][
+    'ObjectStatusEnum']['syntax']['values']
+StateSettingsFlags = MIB_INDEX['IDRAC-MIB-SMIv2'][None][
+    'StateSettingsFlags']['syntax']['values']
+StatusRedundancyEnum = MIB_INDEX['IDRAC-MIB-SMIv2'][None][
+    'StatusRedundancyEnum']['syntax']['values']
+
+
+def state_details(item: dict, metric: str):
+    octet_string = item.pop(metric, None)
+    if octet_string is None:
+        return
+    item[metric] = [
+        StateSettingsFlags.get(o, '?')  # fallback needs to be string
+        for o in octet_string
+    ]
+
+
+def status_details(item: dict, metric: str):
+    octet_string = item.pop(metric, None)
+    if octet_string is None:
+        return
+    item[metric] = [
+        StatusRedundancyEnum.get(o, '?')  # fallback needs to be string
+        for o in octet_string
+    ]
+
+
+def status_list(item: dict, metric: str):
+    octet_string = item.pop(metric, None)
+    if octet_string is None:
+        return
+    item[metric] = [
+        ObjectStatusEnum.get(o, '?')  # fallback needs to be string
+        for o in octet_string
+    ]
 
 
 async def check_idrac(
@@ -14,4 +50,31 @@ async def check_idrac(
         asset_config: dict,
         check_config: dict) -> dict:
     state = await snmpquery(asset, asset_config, check_config, QUERIES)
+
+    for item in state.get('systemStateTableEntry', []):
+        state_details(item, 'systemStatePowerUnitStateDetails')
+        status_details(item, 'systemStatePowerUnitStatusDetails')
+        state_details(item, 'systemStatePowerSupplyStateDetails')
+        status_details(item, 'systemStatePowerSupplyStatusDetails')
+        state_details(item, 'systemStateVoltageStateDetails')
+        status_details(item, 'systemStateVoltageStatusDetails')
+        state_details(item, 'systemStateAmperageStateDetails')
+        status_details(item, 'systemStateAmperageStatusDetails')
+        state_details(item, 'systemStateCoolingUnitStateDetails')
+        status_details(item, 'systemStateCoolingUnitStatusDetails')
+        state_details(item, 'systemStateTemperatureStateDetails')
+        status_details(item, 'systemStateTemperatureStatusDetails')
+        state_details(item, 'systemStateMemoryDeviceStateDetails')
+        status_details(item, 'systemStateMemoryDeviceStatusDetails')
+        state_details(item, 'systemStateChassisIntrusionStateDetails')
+        status_details(item, 'systemStateChassisIntrusionStatusDetails')
+        state_details(item, 'systemStateChassisIntrusionStateDetails')
+        status_details(item, 'systemStateChassisIntrusionStatusDetails')
+        state_details(item, 'systemStateTemperatureStatisticsStateDetails')
+        status_details(item, 'systemStateTemperatureStatisticsStatusDetails')
+        status_list(item, 'systemStateBatteryStatusList')
+        status_list(item, 'systemStateCoolingUnitStatusList')
+        status_list(item, 'systemStatePowerUnitStatusList')
+        status_list(item, 'systemStateProcessorDeviceStatusList')
+
     return state
