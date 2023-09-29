e01ea018af06d4546b4c7b4d341ff4418b3cd7fc
Jeroen van der Heijden <jeroen@cesbit.com>
2023-09-29 13:07:21 +0200

    rm metrics

--- lib/check/idrac.py
+++ lib/check/idrac.py
@@ -8,42 +8,6 @@ QUERIES = (
     MIB_INDEX['IDRAC-MIB-SMIv2']['eventLogTableEntry'],
     MIB_INDEX['IDRAC-MIB-SMIv2']['firmwareTableEntry'],
 )
-ObjectStatusEnum = MIB_INDEX['IDRAC-MIB-SMIv2'][None][
-    'ObjectStatusEnum']['syntax']['values']
-StateSettingsFlags = MIB_INDEX['IDRAC-MIB-SMIv2'][None][
-    'StateSettingsFlags']['syntax']['values']
-StatusRedundancyEnum = MIB_INDEX['IDRAC-MIB-SMIv2'][None][
-    'StatusRedundancyEnum']['syntax']['values']
-
-
-def state_details(item: dict, metric: str):
-    octet_string = item.pop(metric, None)
-    if octet_string is None:
-        return
-    item[metric] = [
-        StateSettingsFlags.get(o, '?')  # fallback needs to be string
-        for o in octet_string
-    ]
-
-
-def status_details(item: dict, metric: str):
-    octet_string = item.pop(metric, None)
-    if octet_string is None:
-        return
-    item[metric] = [
-        StatusRedundancyEnum.get(o, '?')  # fallback needs to be string
-        for o in octet_string
-    ]
-
-
-def status_list(item: dict, metric: str):
-    octet_string = item.pop(metric, None)
-    if octet_string is None:
-        return
-    item[metric] = [
-        ObjectStatusEnum.get(o, '?')  # fallback needs to be string
-        for o in octet_string
-    ]
 
 
 def do_rename(state: dict):
@@ -74,29 +38,29 @@ async def check_idrac(
     state = await snmpquery(asset, asset_config, check_config, QUERIES)
 
     for item in state.get('systemStateTableEntry', []):
-        state_details(item, 'systemStatePowerUnitStateDetails')
-        status_details(item, 'systemStatePowerUnitStatusDetails')
-        state_details(item, 'systemStatePowerSupplyStateDetails')
-        status_details(item, 'systemStatePowerSupplyStatusDetails')
-        state_details(item, 'systemStateVoltageStateDetails')
-        status_details(item, 'systemStateVoltageStatusDetails')
-        state_details(item, 'systemStateAmperageStateDetails')
-        status_details(item, 'systemStateAmperageStatusDetails')
-        state_details(item, 'systemStateCoolingUnitStateDetails')
-        status_details(item, 'systemStateCoolingUnitStatusDetails')
-        state_details(item, 'systemStateTemperatureStateDetails')
-        status_details(item, 'systemStateTemperatureStatusDetails')
-        state_details(item, 'systemStateMemoryDeviceStateDetails')
-        status_details(item, 'systemStateMemoryDeviceStatusDetails')
-        state_details(item, 'systemStateChassisIntrusionStateDetails')
-        status_details(item, 'systemStateChassisIntrusionStatusDetails')
-        state_details(item, 'systemStateChassisIntrusionStateDetails')
-        status_details(item, 'systemStateChassisIntrusionStatusDetails')
-        state_details(item, 'systemStateTemperatureStatisticsStateDetails')
-        status_details(item, 'systemStateTemperatureStatisticsStatusDetails')
-        status_list(item, 'systemStateBatteryStatusList')
-        status_list(item, 'systemStateCoolingUnitStatusList')
-        status_list(item, 'systemStatePowerUnitStatusList')
-        status_list(item, 'systemStateProcessorDeviceStatusList')
+        item.pop('systemStatePowerUnitStateDetails', None)
+        item.pop('systemStatePowerUnitStatusDetails', None)
+        item.pop('systemStatePowerSupplyStateDetails', None)
+        item.pop('systemStatePowerSupplyStatusDetails', None)
+        item.pop('systemStateVoltageStateDetails', None)
+        item.pop('systemStateVoltageStatusDetails', None)
+        item.pop('systemStateAmperageStateDetails', None)
+        item.pop('systemStateAmperageStatusDetails', None)
+        item.pop('systemStateCoolingDeviceStateDetails', None)
+        item.pop('systemStateCoolingDeviceStatusDetails', None)
+        item.pop('systemStateCoolingUnitStateDetails', None)
+        item.pop('systemStateCoolingUnitStatusDetails', None)
+        item.pop('systemStateTemperatureStateDetails', None)
+        item.pop('systemStateTemperatureStatusDetails', None)
+        item.pop('systemStateMemoryDeviceStateDetails', None)
+        item.pop('systemStateMemoryDeviceStatusDetails', None)
+        item.pop('systemStateChassisIntrusionStateDetails', None)
+        item.pop('systemStateChassisIntrusionStatusDetails', None)
+        item.pop('systemStateTemperatureStatisticsStateDetails', None)
+        item.pop('systemStateTemperatureStatisticsStatusDetails', None)
+        item.pop('systemStatePowerUnitStatusList', None)
+        item.pop('systemStateCoolingUnitStatusList', None)
+        item.pop('systemStateProcessorDeviceStatusList', None)
+        item.pop('systemStateBatteryStatusList', None)
 
     return do_rename(state)
