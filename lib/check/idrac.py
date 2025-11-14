ebebd03aec932b5561a2680ea47f2c15aaa35a58
Koos85 <koosjoosten@gmail.com>
2025-11-14 14:10:19 +0100

    libprobe v2

--- lib/check/idrac.py
+++ lib/check/idrac.py
@@ -1,5 +1,6 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from libprobe.asset import Asset
+from libprobe.check import Check
 from libprobe.exceptions import CheckException
 from ..snmpclient import get_snmp_client
 from ..snmpquery import snmpquery
@@ -32,37 +33,39 @@ def do_rename(state: dict):
     return new_state
 
 
-async def check_idrac(
-        asset: Asset,
-        asset_config: dict,
-        check_config: dict) -> dict:
-    snmp = get_snmp_client(asset, asset_config, check_config)
-    state = await snmpquery(snmp, QUERIES)
+class CheckIdrac(Check):
+    key = 'idrac'
 
-    for item in state.get('systemStateTableEntry', []):
-        item.pop('systemStatePowerUnitStateDetails', None)
-        item.pop('systemStatePowerUnitStatusDetails', None)
-        item.pop('systemStatePowerSupplyStateDetails', None)
-        item.pop('systemStatePowerSupplyStatusDetails', None)
-        item.pop('systemStateVoltageStateDetails', None)
-        item.pop('systemStateVoltageStatusDetails', None)
-        item.pop('systemStateAmperageStateDetails', None)
-        item.pop('systemStateAmperageStatusDetails', None)
-        item.pop('systemStateCoolingDeviceStateDetails', None)
-        item.pop('systemStateCoolingDeviceStatusDetails', None)
-        item.pop('systemStateCoolingUnitStateDetails', None)
-        item.pop('systemStateCoolingUnitStatusDetails', None)
-        item.pop('systemStateTemperatureStateDetails', None)
-        item.pop('systemStateTemperatureStatusDetails', None)
-        item.pop('systemStateMemoryDeviceStateDetails', None)
-        item.pop('systemStateMemoryDeviceStatusDetails', None)
-        item.pop('systemStateChassisIntrusionStateDetails', None)
-        item.pop('systemStateChassisIntrusionStatusDetails', None)
-        item.pop('systemStateTemperatureStatisticsStateDetails', None)
-        item.pop('systemStateTemperatureStatisticsStatusDetails', None)
-        item.pop('systemStatePowerUnitStatusList', None)
-        item.pop('systemStateCoolingUnitStatusList', None)
-        item.pop('systemStateProcessorDeviceStatusList', None)
-        item.pop('systemStateBatteryStatusList', None)
+    @staticmethod
+    async def run(asset: Asset, local_config: dict, config: dict) -> dict:
 
-    return do_rename(state)
+        snmp = get_snmp_client(asset, local_config, config)
+        state = await snmpquery(snmp, QUERIES)
+
+        for item in state.get('systemStateTableEntry', []):
+            item.pop('systemStatePowerUnitStateDetails', None)
+            item.pop('systemStatePowerUnitStatusDetails', None)
+            item.pop('systemStatePowerSupplyStateDetails', None)
+            item.pop('systemStatePowerSupplyStatusDetails', None)
+            item.pop('systemStateVoltageStateDetails', None)
+            item.pop('systemStateVoltageStatusDetails', None)
+            item.pop('systemStateAmperageStateDetails', None)
+            item.pop('systemStateAmperageStatusDetails', None)
+            item.pop('systemStateCoolingDeviceStateDetails', None)
+            item.pop('systemStateCoolingDeviceStatusDetails', None)
+            item.pop('systemStateCoolingUnitStateDetails', None)
+            item.pop('systemStateCoolingUnitStatusDetails', None)
+            item.pop('systemStateTemperatureStateDetails', None)
+            item.pop('systemStateTemperatureStatusDetails', None)
+            item.pop('systemStateMemoryDeviceStateDetails', None)
+            item.pop('systemStateMemoryDeviceStatusDetails', None)
+            item.pop('systemStateChassisIntrusionStateDetails', None)
+            item.pop('systemStateChassisIntrusionStatusDetails', None)
+            item.pop('systemStateTemperatureStatisticsStateDetails', None)
+            item.pop('systemStateTemperatureStatisticsStatusDetails', None)
+            item.pop('systemStatePowerUnitStatusList', None)
+            item.pop('systemStateCoolingUnitStatusList', None)
+            item.pop('systemStateProcessorDeviceStatusList', None)
+            item.pop('systemStateBatteryStatusList', None)
+
+        return do_rename(state)
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from libprobe.probe import Probe
-from lib.check.idrac import check_idrac
+from lib.check.idrac import CheckIdrac
 from lib.version import __version__ as version
 
 
 if __name__ == '__main__':
-    checks = {
-        'idrac': check_idrac,
-    }
+    checks = (
+        CheckIdrac,
+    )
 
     probe = Probe("idrac", version, checks)
 
